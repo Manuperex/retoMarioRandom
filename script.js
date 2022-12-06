1730862f87103b8e47a1d726e678889bea7d0485
Manuperex <104181677+Manuperex@users.noreply.github.com>
2022-12-06 17:26:51 -0500

    se agrego la funcionalidad sobre  eliminar random y eliminar li

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
-
+const marioContainer = document.querySelector(".mario-container");
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const text = input.value;
 
   if (text != "") {
     const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.textContent = text;
-    li.appendChild(p);
+    // const p = document.createElement("p");
+    // p.className = "parrafo-li";
+    li.className = "li-name"
+    // p.textContent = text;
+    li.textContent = text;
     li.appendChild(addDeleteBtn());
     ul.appendChild(li);
 
@@ -35,3 +37,70 @@ function addDeleteBtn() {
 
   return deleteBtn;
 }
+
+let arrayUser=[];
+let nombreAleatorio ="";
+const addBtnLet = document.querySelector(".btn-lets-go");
+addBtnLet.addEventListener("click", (e) => { 
+  arrayUser = [];
+  const valor=document.querySelectorAll(".li-name") 
+  for(let i=0; i<valor.length; i++){ 
+    arrayUser.push(valor[i].textContent); 
+  } 
+  console.log(arrayUser); 
+  // console.log(randomUser(arrayUser));
+  e.preventDefault(); 
+  nombreAleatorio = randomUser(arrayUser);
+  console.log(nombreAleatorio);
+  console.log(arrayUser);
+  arrayUser = deleteUser(arrayUser, nombreAleatorio);
+  e.preventDefault(); 
+  // nombreAleatorio = randomUser(arrayUser);
+  console.log(nombreAleatorio);
+  console.log(arrayUser);
+});
+
+function randomUser(arrayUser){
+  const aleatorio = arrayUser[Math.floor(Math.random() * arrayUser.length)];
+  return aleatorio
+}
+
+
+function deleteUser(arrayUser, nombre){
+  const filteredLibraries = arrayUser.filter((item) => item !== nombre)
+  const valor = document.querySelectorAll(".li-name");
+  if(valor.length === 1){
+    swal ( "Ganaste " + nombre );
+  }
+  else{
+    swal ( "Perdiste " + nombre );
+  }
+  for(let i=0; i<valor.length; i++){  
+    // console.log(valor[i].textContent);
+    if(nombre === valor[i].textContent){
+      ul.removeChild(valor[i])
+      
+    }
+  }
+  return filteredLibraries
+}
+// const start = document.querySelector(".btn-lets-go-2");
+// start.addEventListener("click", (e) => {
+  
+  
+  
+
+//   }
+// );
+// console.log(nombres.length)
+// for (let i = 0; i <= nombres.length; i++) {
+//   console.log(i)
+//   console.log(nombres);
+//   console.log(nombres.length);
+//   let nameRandom = randomUser(nombres);
+//   console.log(nameRandom);
+//   nombres = deleteUser(nombres, nameRandom);
+//   console.log(nombres);
+// }
+
+
